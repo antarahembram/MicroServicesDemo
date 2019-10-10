d9cc137a3b15d3422dadd8ac2e9bcbf8f11ab0c7
antara.hembram <antara.hembram@cgi.com>
2019-10-10 19:29:14 +0530

    adding userApplication,apiway

---
+++ userApplication/src/main/java/com/stackroute/domain/User.java
@@ -0,0 +1,45 @@
+package com.stackroute.domain;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    private int id;
+    private String firstName;
+    private String lastName;
+
+    public User() {
+    }
+
+    public User(int id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}
